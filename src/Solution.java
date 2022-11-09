e0c89d5090003e256b544b54218fcd6a2ae1524d
sakhnopy <daniilsahnov@mail.ru>
2022-11-09 22:53:57 +0300

    dd

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,24 @@
 import java.lang.reflect.InvocationTargetException;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Solution {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Connections connections = new Connections();
         Statement statement = connections.DbConnection().createStatement();
         ResultSet rs =  statement.executeQuery("select * from people");
+        List<String> stringList = new ArrayList<>();
         while (rs.next()) {
-            rs.getString("name");
-            rs.getString("age");
-            System.out.println(rs.getString("age") + " " + rs.getString("name"));
-            System.out.println();
+            stringList.add(rs.getString("age") + " " + rs.getString("name") + " " + rs.getString("email"));
         }
-    }
 
+        stringList.stream()
+                .filter(name -> name.contains("2"))
+                .sorted(Comparator.comparing(ss -> ss.toLowerCase()))
+                .forEach(System.out::println);
+
+    }
 }
